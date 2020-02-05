84ea7e6acf4d3fdeb336da0ecfe4419166ad4254
Tina Oberoi <oberoi011115tina@gmail.com>
2020-02-05 13:25:22 +0530

    Add backtracking questions

---
+++ imp-probs/backtracking/nqueens.cpp
@@ -0,0 +1,82 @@
+/*
+You are given N, and for a given N x N chessboard, find a way to place N queens such that no queen can attack any other queen on the chess board. A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. You have to print all such configurations.
+Input Format :
+Line 1 : Integer N
+Output Format :
+One Line for every board configuration. 
+Every line will have N*N board elements printed row wise and are separated by space
+Note : Don't print anything if there isn't any valid configuration.
+Constraints :
+1<=N<=10
+Sample Input 1:
+4
+Sample Output 1 :
+0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 
+0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> board;
+
+bool isPossible(int n, int row, int col){
+    for(int i =row-1; i>=0; i--){
+        if(board[i][col] == 1)
+            return false;
+    }
+
+    for(int i = row-1, j = col-1; i>=0&&j>=0; i--, j--){
+        if(board[i][j] == 1)
+            return false;
+    }
+
+    for(int i = row-1, j = col+1; i>=0&&j<n; i--, j++){
+        if(board[i][j] == 1)
+            return false;        
+    }
+    return true;
+}
+
+void placeQueensHelper(int n, int row){
+    if( row == n){
+        for(int i =0; i<n; i++){
+            for(int j =0; j<n; j++){
+                cout<<board[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+        return;
+    }
+
+    for(int j =0; j<n; j++){
+        if(isPossible(n, row, j)){
+            board[row][j] = 1;
+            placeQueensHelper(n, row+1);
+            board[row][j] = 0;
+        }
+    }
+    return;
+}
+
+void placeNQueens(int n){
+
+  /* Don't write main().
+   *  Don't read input, it is passed as function argument.
+   *  Print output as specified in the question
+  */
+    board.clear();
+    board.resize(n, vector<int> (n, 0));
+    placeQueensHelper(n, 0);
+
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    board.clear();
+    board.resize(n, vector<int> (n, 0));
+    placeNQueens(n);
+
+}
---
+++ imp-probs/backtracking/ratInMaze.cpp
@@ -0,0 +1,48 @@
+bool notValid(int maze[][20], int n, int x, int y, int solution[20][20]){
+    if(x>=n || y>=n || solution[x][y] == 1 || maze[x][y] == 0 || x<0 || y<0)
+        return true;
+    
+    return false;
+}
+
+void printSol(int solution[20][20], int n){
+    for(int i=0; i<n; i++){
+        for(int j =0; j<n; j++){
+            cout<<solution[i][j]<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+void helper(int maze[][20], int n, int x, int y, int solution[20][20]){
+    if(x == n-1 && y == n-1){
+        solution[x][y] = 1;
+        printSol(solution, n);
+    }
+    
+    if(notValid(maze, n, x, y, solution))
+        return;
+    
+    solution[x][y] = 1;
+    helper(maze, n, x+1, y, solution);
+    helper(maze, n, x, y+1, solution);
+    helper(maze, n, x-1, y, solution);
+    helper(maze, n, x, y-1, solution);
+    solution[x][y] = 0;
+}
+
+void ratInAMaze(int maze[20][20], int n){
+
+  /* Don't write main().
+   *  Don't read input, it is passed as function argument.
+   *  Print output as specified in the question
+  */
+    int solution[20][20];
+    for(int i=0; i<20; i++){
+        for(int j =0; j<20; j++){
+            solution[i][j]= 0;
+        }
+    }
+    helper(maze, n, 0, 0, solution);
+
+}
