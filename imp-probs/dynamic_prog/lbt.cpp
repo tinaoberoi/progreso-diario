d6c2913d8dfa43210e1661cf69c15ca0e50a40b2
Tina Oberoi <oberoi011115tina@gmail.com>
2020-02-08 22:52:45 +0530

    Add lbt problem

---
+++ imp-probs/dynamic_prog/lbt.cpp
@@ -0,0 +1,64 @@
+/*
+You are given an array of positive integers as input. Write a code to return the length of the largest such subsequence in which the values are arranged first in strictly ascending order and then in strictly descending order.
+Such a subsequence is known as bitonic subsequence. A purely increasing or purely decreasing subsequence will also be considered as a bitonic sequence with the other part empty.
+Note that the elements in bitonic subsequence need not be consecutive in the given array but the order should remain same.
+Input Format:
+Line 1 : A positive Integer N, i.e., the size of array
+Line 2 : N space-separated integers as elements of the array 
+Output Format:
+Length of Largest Bitonic subsequence
+Input Constraints:
+1<= N <= 100000
+Sample Input 1:
+6
+15 20 20 6 4 2
+Sample Output 1:
+5
+Sample Output 1 Explanation:
+Here, longest Bitonic subsequence is {15, 20, 6, 4, 2} which has length = 5.
+Sample Input 2:
+2
+1 5
+Sample Output 2:
+2
+Sample Input 3:
+2
+5 1
+Sample Output 3:
+2
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    vector<int> dp_d(n, 1);
+    vector<int> dp_i(n, 1);
+
+    for(int i =1; i<n; i++){
+        for(int j = 0; j<i; j++){
+            if(v[i]>v[j] && dp_i[i] < dp_i[j] + 1){
+                dp_i[i] = dp_i[j] + 1;
+            }
+        }
+    }
+
+    for(int i = n-2; i>0; i--){
+        for(int j = n-1; j>i; j--){
+            if(v[i] > v[j] && dp_d[i] < dp_d[j] + 1){
+                dp_d[i] = dp_d[j] + 1; 
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i =0; i<n; i++){
+        ans = max(ans, dp_d[i]+dp_i[i] - 1);
+    }
+
+    cout << ans<<endl;
+    return 0;
+}
