9aa79cfafd30ffa984079f558b74444296e76b8b
Tina Oberoi <oberoi011115tina@gmail.com>
2020-04-18 05:09:29 +0530

    Add level1, leve2

--- python3/projects/function_exc.py
+++ python3/projects/function_exc.py
@@ -26,18 +26,97 @@ def makes_twenty(n1,n2):
 
 # Level1
 def old_macdonald(name):
-	ans = ''
-	count = 0
-	for s in name:
-		print(s)
-		if(count == 0 and count == 3):
-			ans+=s.upper()
-			print(ans)
-			count+=1
-		else:
-			ans+=s
-			print(ans)
-			count+=1
-	return ans
+	return name[:3].capitalize()+name[3:].capitalize()
 
-print(old_macdonald('macdonald'))
+#print(old_macdonald('macdonald'))
+
+def master_yoda(text):
+	b = text[::-1]
+	my_list=b.split()
+	res = ""
+	for i in my_list:
+		res+=i[::-1]
+		res+=" "
+	return res
+
+#print(master_yoda('I am home'))
+#print(master_yoda('We are ready'))
+
+def almost_there(n):
+	low1 = 90
+	low2 = 190
+	high1 = 110
+	high2 = 210
+	if(low1<=n<=high1 or low2<=n<=high2):
+		return True
+	return False
+
+#print(almost_there(209))
+
+#Level2
+def has_33(nums):
+	l = len(nums)
+	index = 0
+	for i, x in enumerate(nums):
+		if(x == 3):
+			index = i
+			break
+	#print(i)
+	if(i==l-1):
+		return False
+	if(i<l and nums[i+1] == 3):
+		return True
+	return False
+
+#print(has_33([3, 1, 3]))
+
+def paper_doll(text):
+	res = ""
+	for char in text:
+		res+=char*3
+	return res
+#print(paper_doll('Hello'))
+
+def blackjack(a, b, c):
+	s = sum((a, b, c)) 
+	if(s <= 21):
+		return s;
+	if(s >21 and 11 in (a, b, c)):
+		return s-10
+	else:
+		return 	"BUST"
+
+#print(blackjack(9,9,9))
+
+#Challenging Problems
+
+def spy_game(nums):
+	index = 0
+	for i, n in enumerate(nums):
+		if(n == 0):
+			index = i
+			break
+	if(nums[index:index+3] == [0, 0, 7]):
+		return True
+	return False
+#print(spy_game([1,0,2,4,0,5,7]))
+
+import math
+
+def ifprime(num):
+	if(num%2 == 0 and num>2):
+		return False
+	for i in range(3, sqrt(num)+1, 2):
+		if(num%i == 0):
+			return False
+	return True
+
+def count_primes(num):
+	count = 0;
+	for i in num:
+		if(ifprime(i)):
+			count+=1
+	return count
+
+print(count_primes(100))
+	
