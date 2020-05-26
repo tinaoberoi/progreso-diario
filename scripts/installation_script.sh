3795c7c1e183d73368e0b36bc69631f6b30c1c39
tina <oberoi011115tina@gmail.com>
2020-05-26 13:47:31 +0530

    Add sublime and git installation

---
+++ scripts/installation_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Installing git -----------------------------------------------------"
+sudo apt install git
+echo "Configuring git ----------------------------------------------------"
+git config --global user.email "oberoi011115tina@gmail.com"
+git config --global user.name "tina"
+
+# Install syblime
+echo "Installing sublime -----------------------------------------------------"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
+sudo apt install sublime-text
+# start subl using command line
+#sudo ln -s /opt/sublime/sublime_text /usr/bin/subl
