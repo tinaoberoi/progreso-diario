b98fc69939ec3053f1500368e0aa86944d5ec2bf
tina <oberoi011115tina@gmail.com>
2020-05-27 11:54:45 +0530

    Add virtualbox to script

--- scripts/installation_script.sh
+++ scripts/installation_script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 echo "Installing git -----------------------------------------------------"
-sudo apt install git
-echo "Configuring git ----------------------------------------------------"
-git config --global user.email "oberoi011115tina@gmail.com"
-git config --global user.name "tina"
+#sudo apt install git
+#echo "Configuring git ----------------------------------------------------"
+#git config --global user.email "oberoi011115tina@gmail.com"
+#git config --global user.name "tina"
 
 # Install syblime
 echo "Installing sublime -----------------------------------------------------"
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo apt update
-sudo apt install sublime-text
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+#sudo apt update
+#sudo apt install sublime-text
 # start subl using command line
 #sudo ln -s /opt/sublime/sublime_text /usr/bin/subl
+sudo apt-get install terminator
